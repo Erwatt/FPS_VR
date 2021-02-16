51c0200fa525aae6b64f7bdd924d56929af739fe
Antoine <antoinea723@gmail.com>
2021-02-16 15:14:05 +0100

    Correction erreur git

--- FPS_VR/Assets/Scripts/ZombieController.cs
+++ FPS_VR/Assets/Scripts/ZombieController.cs
@@ -7,7 +7,8 @@ public class ZombieController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent zombie;
     [SerializeField] private Animator animZombie;
-    
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +25,7 @@ public class ZombieController : MonoBehaviour
             zombie.transform.rotation = Quaternion.Slerp(zombie.transform.rotation, Quaternion.LookRotation(direction),0.1f);
         }
 
-        if (Vector3.Distance(WeaponInstance.posPlayer, zombie.transform.position) > 0.5)
+        if (Vector3.Distance(WeaponInstance.posPlayer, zombie.transform.position) >= 1f)
         {
             animZombie.SetBool("nearPlayer",false);
             zombie.SetDestination(WeaponInstance.posPlayer);
@@ -33,5 +34,31 @@ public class ZombieController : MonoBehaviour
         {
             animZombie.SetBool("nearPlayer",true);
         }
+
+        if (animZombie.GetBool("nearPlayer"))
+        {
+            attackController();
+        }
+    }
+    
+    //private void OnTriggerEnter(Collider other)
+    //   {
+    //       Debug.Log("prout");
+    //Destroy(other.gameObject);
+    //   }
+
+    private void attackController()
+    {
+        const float attackDelay = 3f;
+         
+        if (timer > attackDelay)
+        {
+            animZombie.SetBool("attack",true);
+            Debug.Log("player takes damage");
+            timer = 0f;
+        }
+
+        timer += Time.deltaTime;
+         
     }
 }
