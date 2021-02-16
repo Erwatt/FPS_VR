5ad15d3a5821f4a3c9de90df930ade1c984fdcf0
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2021-02-16 14:48:37 +0100

    Zombie attack anim

--- FPS_VR/Assets/Scripts/ZombieController.cs
+++ FPS_VR/Assets/Scripts/ZombieController.cs
@@ -9,6 +9,7 @@ public class ZombieController : MonoBehaviour
 
     [SerializeField] private NavMeshAgent zombie;
     [SerializeField] private Animator animZombie;
+    private float timer;
     
     // Start is called before the first frame update
     void Start()
@@ -26,7 +27,7 @@ public class ZombieController : MonoBehaviour
             zombie.transform.rotation = Quaternion.Slerp(zombie.transform.rotation, Quaternion.LookRotation(direction),0.1f);
         }
 
-        if (Vector3.Distance(playerPos.position, zombie.transform.position) > 0.5)
+        if (Vector3.Distance(playerPos.position, zombie.transform.position) >= 1f)
         {
             animZombie.SetBool("nearPlayer",false);
             zombie.SetDestination(playerPos.position);
@@ -35,5 +36,32 @@ public class ZombieController : MonoBehaviour
         {
             animZombie.SetBool("nearPlayer",true);
         }
+
+        if (animZombie.GetBool("nearPlayer") == true)
+        {
+            attackController();
+        }
     }
+
+    //private void OnTriggerEnter(Collider other)
+     //   {
+     //       Debug.Log("prout");
+            //Destroy(other.gameObject);
+     //   }
+
+     private void attackController()
+     {
+         const float attackDelay = 3f;
+         
+         if (timer > attackDelay)
+         {
+             animZombie.SetBool("attack",true);
+             Debug.Log("player takes damage");
+             animZombie.SetBool("attack", false);
+             timer = 0f;
+         }
+
+         timer += Time.deltaTime;
+         
+     }
 }
